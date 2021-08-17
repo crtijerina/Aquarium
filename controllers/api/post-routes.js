98630adf0eb6fde5bcdfd1065860630dd8f8c86c
James Bowen (J.D.) <j.bowen1821@gmail.com>
2021-08-17 04:43:25 -0500

    added post routes for api

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,135 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+    //   attributes: [
+    //     'id',
+    //     'contents',
+    //     'title',
+    //     'created_at',
+    //   ],
+    //   include: [
+    //     {
+    //       model: Comment,
+    //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //       include: {
+    //         model: User,
+    //         attributes: ['username']
+    //       }
+    //     },
+    //     {
+    //       model: User,
+    //       attributes: ['username']
+    //     }
+    //   ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+    //   attributes: [
+    //     'id',
+    //     'contents',
+    //     'title',
+    //     'created_at',
+    //   ],
+    //   include: [
+    //     {
+    //       model: Comment,
+    //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //       include: {
+    //         model: User,
+    //         attributes: ['username']
+    //       }
+    //     },
+    //     {
+    //       model: User,
+    //       attributes: ['username']
+    //     }
+    //   ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.post('/', withAuth, (req, res) => {
+    // expects {title: 'Comment Post', contents: 'Posting data', user_id: 1}
+    Post.create({
+    //   title: req.body.title,
+    //   contents: req.body.contents,
+    //   user_id: req.session.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  
+  router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.delete('/:id', withAuth, (req, res) => {
+    console.log('id', req.params.id);
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  module.exports = router;
