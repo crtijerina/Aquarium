94429abbabe5ee6a153e373ba2e67bcaa8e7f937
James Bowen (J.D.) <j.bowen1821@gmail.com>
2021-08-22 13:32:05 -0500

    added posting and comment js

---
+++ public/JS/add-post.js
@@ -0,0 +1,26 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const contents = document.querySelector('input[name="contents"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        contents
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/aquarium');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  
---
+++ public/JS/comment.js
@@ -0,0 +1,30 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+  
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          post_id,
+          comment_text
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+  
